b5ede3fd7d41674d42ce32b85c988423d1d34447
Emilse <emilsearias@gmail.com>
2021-12-09 12:27:19 -0300

    se agrego la estructura del proyecto de backend

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
 //creo una instancia de express
 const app = express();
 //crear un puerto para el servidor
@@ -8,4 +11,17 @@ app.listen(app.get('port'),()=>{
     console.log('Estoy en el puerto '+ app.get('port'));
 })
 
-console.log('prueba')
+//midlewares o configuraciones extras
+app.use(morgan('dev')); //da informacion extra a la terminal
+app.use(cors());//permite recibir peticiones remotas
+//permite recibir objetos en formato json
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//crear rutas
+app.get('/', (req,res)=>{
+    res.send('hola desde el servidor backend')
+});
+app.delete('/borrardatos', (req,res)=>{
+    res.send('aqui vamos a borrar')
+});
