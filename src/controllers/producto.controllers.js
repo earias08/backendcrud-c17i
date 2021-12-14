b9e208e9a169528140bb3edb20e50189d50f4e75
Emilse <emilsearias@gmail.com>
2021-12-14 18:36:28 -0300

    se agrego la logica para agregar productos

--- src/controllers/producto.controllers.js
+++ src/controllers/producto.controllers.js
@@ -1,3 +1,5 @@
+import Producto from "../models/producto";
+
 const productoCtrl ={};
 
 //logica para obtener la lista de productos
@@ -9,9 +11,29 @@ productoCtrl.borrarProducto = (req, res)=>{
     res.send('aqui borro los productos')
 }
 
-productoCtrl.crearProducto = (req, res)=>{
-    console.log(req.body);
-    res.send('aqui quiero crear productos');
+productoCtrl.crearProducto = async(req, res)=>{
+    // console.log(req.body);
+    try{
+        //validar
+        //crear un nuevo objeto para guardar en la BD
+        const productoNuevo = new Producto({
+            nombreProducto: req.body.nombreProducto,
+            precioProducto: req.body.precioProducto,
+            categoria: req.body.categoria
+        });
+        // guardar producto en BD
+        await productoNuevo.save();
+        //enviar la respuesta
+        res.status(201).json({
+            mensaje: 'Producto agregado correctamente'
+        })
+    }catch(error){
+        console.log(error);
+        //enviar codigo de error
+        res.status(400).json({
+            mensaje:'Error al agregar un producto'
+        })
+    }
 }
 
 export default productoCtrl
