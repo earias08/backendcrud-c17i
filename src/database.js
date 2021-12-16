a9089097223ca0378623f20b220c63670d2e8fb5
Emilse <emilsearias@gmail.com>
2021-12-16 18:16:35 -0300

    cambiamos la ruta de la bd a mongo atlas

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 //cadena de conexion a la base de datos
-const url = 'mongodb://localhost:27017/crudcafec17i';
+// const url = 'mongodb://localhost:27017/crudcafec17i';
+const url = 'mongodb+srv://emic17i:cafecrud17i@cluster0.4oulm.mongodb.net/crudcafec17i';
 
 mongoose.connect(url,{useNewUrlParser:true});
 
